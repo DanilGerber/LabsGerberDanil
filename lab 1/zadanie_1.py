879e3f1dcb1dfafd86b1134548a00cc9ab780d62
DanilGerber <danilgerber555@gmail.com>
2024-11-12 17:49:47 +0500

    Update zadanie_1.py
    Исправил решение, решил без set() так как и правда могут быть повторения ведь

--- lab 1/zadanie_1.py
+++ lab 1/zadanie_1.py
@@ -1,9 +1,8 @@
-numbers_list = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
+numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-numbers_list[4] = 0
-numbers = set(numbers_list)
+numbers[4] = 0
 average = sum(numbers) / len(numbers)
-numbers_list[4] = average
+numbers[4] = average
 
-print("Измененный список:", numbers_list)
+print("Измененный список:", numbers)
